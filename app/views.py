06383fc5a3f03e190a8ce56e0548ac8b89a2d9a9
wellesleywatson <git@github.com:wellesleywatson>
2015-02-15 21:45:29 +0000

    security update2

--- app/views.py
+++ app/views.py
@@ -56,7 +56,8 @@ def sendemail(faddr, taddr, msend):
   
   import smtplib
   #Credentials (if needed)
-
+  username = 'nostaw7@gmail.com'
+  password = 'not gonna put that'
 
   # The actual mail send
   server = smtplib.SMTP('smtp.gmail.com:587')
